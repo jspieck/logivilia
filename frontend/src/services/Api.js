055013693e1549bc8c0162a3b7a2feef344cf1c8
Jan Spieck <jspnbg@web.de>
2020-07-12 17:26:27 +0200

    Localhost setting API

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import store from '@/store/store'
 
+// http:localhost:8081
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8081`,
+    baseURL: `https://logivilia.com`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
